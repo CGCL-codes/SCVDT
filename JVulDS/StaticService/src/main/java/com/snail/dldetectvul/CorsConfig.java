db451228233a64618378117704c3df92f451f7a3
ZhangKe <4everTomchristmas@gmail.com>
2021-06-14 22:52:47 +0800

    Update CorsConfig.java

--- JVulDS/StaticService/src/main/java/com/snail/dldetectvul/CorsConfig.java
+++ JVulDS/StaticService/src/main/java/com/snail/dldetectvul/CorsConfig.java
@@ -9,15 +9,15 @@ import org.springframework.web.filter.CorsFilter;
 public class CorsConfig {
     private CorsConfiguration buildConfig() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("*"); //允许任何域名
-        corsConfiguration.addAllowedHeader("*"); //允许任何头
-        corsConfiguration.addAllowedMethod("*"); //允许任何方法
+        corsConfiguration.addAllowedOrigin("*"); 
+        corsConfiguration.addAllowedHeader("*"); 
+        corsConfiguration.addAllowedMethod("*"); 
         return corsConfiguration;
     }
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", buildConfig()); //注册
+        source.registerCorsConfiguration("/**", buildConfig()); 
         return new CorsFilter(source);
     }
 }
