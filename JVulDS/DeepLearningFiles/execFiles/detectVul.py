b33f9d1b5236ec8223c49cc1df828d54e1d72cf2
ZhangKe <4everTomchristmas@gmail.com>
2021-06-14 22:20:15 +0800

    Update detectVul.py

--- JVulDS/DeepLearningFiles/execFiles/detectVul.py
+++ JVulDS/DeepLearningFiles/execFiles/detectVul.py
@@ -12,8 +12,8 @@ from train_w2vmodel import generate_w2vModel, get_input_dl, get_all_dl
 
 """
 input: 
-1）文件位置
-2) 存放的固定文件的位置
+1）filePath
+2) own filePath
 """
 
 projPath = sys.argv[1]
@@ -27,18 +27,17 @@ resultPath = projPath + os.path.sep + "result.txt"
 fixFilePath = sys.argv[2]
 w2v_input_path = fixFilePath + os.path.sep + "word2vec_input"
 
-# step 1: 获取切片 得到slice
+# step 1:get slice
 start(slicePath)
 #
-# step 2: 获取word2vec的输入 得到corpus
+# step 2: get the input of word2vec and get corpus
 get_sentences(slicePath, corpusPath, mapType=True)
 
-# # # step 3: 训练train_w2v并获取dl的输入 得到vector
+# # # step 3: get vector
 dec_tokenFlaw_path = [corpusPath, w2v_input_path]
 generate_w2vModel(dec_tokenFlaw_path, w2v_model_path)
 get_input_dl(corpusPath, w2v_model_path, vectorPath)
 get_all_dl(vectorPath, dlCorpusPath)
-# #
-# # step 4: 预测结果
+# #predict result
 predict(dlCorpusPath, fixFilePath + os.path.sep, resultPath)
 
