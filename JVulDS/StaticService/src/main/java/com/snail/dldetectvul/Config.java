8353d891b68d94e6811894743551a88d670aac54
ZhangKe <4everTomchristmas@gmail.com>
2021-06-14 22:52:14 +0800

    Update Config.java

--- JVulDS/StaticService/src/main/java/com/snail/dldetectvul/Config.java
+++ JVulDS/StaticService/src/main/java/com/snail/dldetectvul/Config.java
@@ -1,12 +1,12 @@
 package com.snail.dldetectvul;
 
 public class Config {
-    //hj 配置文件
-    // 存放python文件的路径
+    //hj 
+    // python 
     public static final String pythonEnvPath = "/Library/Frameworks/Python.framework/Versions/3.8/bin/python3";
-    // 存放python代码 + word2vec交叉验证 + 深度模型
+    // 
     public static final String fixedFilePath = "/Users/ke/Documents/snail/graduate/platform/serverTest/DeepLearningFiles";
-    // 另外一个服务端地址
+    // PDG Server url
     public static final String fileServiceUrl="http://127.0.0.1:8889/pdg";
 
 
